c5dfa409045701b3d4a60049c0d252667d91b691
kvora2 <kelvinvora21@gmail.com>
2023-06-14 01:37:28 -0400

    finalized

--- server.js
+++ server.js
@@ -1,15 +1,6 @@
 /*********************************************************************************
-*  BTI425 – Assignment 2
-*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
-*  No part of this assignment has been copied manually or electronically from any other source
-*  (including web sites) or distributed to other students.
-* 
-*  Name: Kelvin Vora Student 
-*  ID: 157616210 
-*  Date: 31st Jan 2023
-*
 *  Cyclic Link: 
-*  https://panicky-boa-pullover.cyclic.app 
+*  
 ********************************************************************************/
 
 const express = require('express')
